739d01ce293ca6bd8019e57ca77e3a651fa2f826
Rodrigo Moisés <93003716+202108679@users.noreply.github.com>
2022-12-20 17:10:57 +0000

    Delete ok/Gatilhos directory

--- ok/Gatilhos/gatilho1_verifica.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO Jogador (nomeJogador, numero, nacionalidade, totalGolos, nomeEquipa) VALUES ('B. Folha', 87, 'Portugues', NULL,'FC Porto');
-
-INSERT INTO Jogador (nomeJogador, numero, nacionalidade, totalGolos, nomeEquipa) VALUES ('Pintassilgo', 98, 'Portugues', 3,'FC Vizela');
-
-SELECT * FROM Jogador;
